3dd92a4f603ee156bc35f64ecc0cc904fa22de4d
aavram <aavram27@gmail.com>
2017-10-21 20:28:51 -0700

    user page

---
+++ home.php
@@ -0,0 +1,66 @@
+  <!DOCTYPE html>
+  <html>
+    <head>
+
+        <link href="myown.css" rel="stylesheet" />
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <!--Import jQuery before materialize.js-->
+
+    
+<!--
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo right">Logo</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">My Listings</a></li>
+                    <li><a href="">Saved Listings</a></li>
+                    <li><a href="">Drafts</a></li>
+                </ul>
+            </div>
+        </nav>
+-->
+        
+        <!--<div class="w3-sidebar w3-bar-block" style="width:25%"> 
+            <a href="#" class="w3-bar-item w3-button">Link 1</a>
+            <a href="#" class="w3-bar-item w3-button">Link 2</a>
+            <a href="#" class="w3-bar-item w3-button">Link 3</a>
+        </div>
+
+        <div style="margin-left:25%">
+            ... page content ...
+        </div>-->
+        <div class="container-fluid" >
+          <div class="row">
+            <div class="col-sm-3 col-lg-2 navBarDiv">
+                <div class="row">
+                <span class="glyphicon glyphicon-user icon" aria-hidden="true"></span>
+                </div>
+              <nav class="nav nav-pills nav-stacked leftNavbar">
+                <li><a href="">Profile</a></li>
+                <li class="active"><a href="">Dashboard</a></li>
+                <li><a href="">My Listings</a></li>
+                <li><a href="">Saved Listings</a></li>
+                <li><a href="">Drafts</a></li>
+              </nav>
+            </div>
+            <div class="col-sm-9 col-lg-10">
+                <div class="row header">
+                
+                </div>
+              <!-- your page content -->
+            </div>
+          </div>
+        </div>
+              <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+    </body>
+  </html>
