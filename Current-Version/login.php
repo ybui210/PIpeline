d5c4dfb9a9c68a9c6d55a8da13aa79e38da004d6
Davin Deol <davindeol@gmail.com>
2017-10-22 20:29:31 -0700

    Updated login pages and started implementing PHP

--- Current-Version/login.php
+++ Current-Version/login.php
@@ -4,45 +4,67 @@
         <title>Login</title> <!-- Decided by Davin, open to change -->
         <link rel="stylesheet" type="text/css" href="Styles/login.css">
         
-        <!-- All this favicon code was taken from https://www.favicon-generator.org/ -->
-        <link rel="apple-touch-icon" sizes="57x57" href="./Images/Favicon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="./Images/Favicon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="./Images/Favicon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="./Images/Favicon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="./Images/Favicon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="./Images/Favicon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="./Images/Favicon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="./Images/Favicon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="./Images/Favicon/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="./Images/Favicon/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./Images/Favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="./Images/Favicon/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./Images/Favicon/favicon-16x16.png">
-        <link rel="manifest" href="./Images/Favicon/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
+        <?php
+            include 'favicon.php';
+        ?>
     </header>
     <body>
         <div id="header"> <!-- To hold the login buttons and labels that take you to different sections of the page -->
         </div>
         <!-- Holds all objects not in the header -->
         <div id="content">
-            <h1>Login</h1>
-            <form class="cf">
+            <h1>
+                <?php 
+                    $forgotPassword = false;
+                    $requestInvitation = false;
+                    if (isset($_POST["forgotPassword"])) {
+                        echo "Forgot Password";
+                        $forgotPassword = true;
+                    } else if (isset($_POST["requestInvitation"])) {
+                        echo "Request Invitation";
+                        $requestInvitation = true;
+                    } else {
+                        echo "Login";
+                    } 
+                ?>
+            </h1>
+            <form class="cf" method="post" action="">
                 <div id="formBox">
-                    <div class="half left cf">
-                        <input type="email" id="input-email" placeholder="Email"><br>
-                        <input type="password" id="input-name" placeholder="Password">
-                    </div>
-                    <div id="rememberMeSection">
-                        <input type="checkbox" id="rememberMe"/>
-                        <label for="rememberMe">Remember Me</label>
-                    </div>
+                    <input type="email" id="input-email" placeholder="Email"><br>
+                    <?php 
+                        if ($forgotPassword) {
+                            echo "";
+                        } else if($requestInvitation) {
+                            echo "<input type=\"text\" id=\"input-first-name\" placeholder=\"First Name\">";
+                            echo "<input type=\"text\" id=\"input-middle-name\" placeholder=\"Middle Name\">";
+                            echo "<input type=\"text\" id=\"input-last-name\" placeholder=\"Last Name\">";
+                            echo "<input type=\"text\" id=\"input-linkedin-url\" placeholder=\"LinkedIn URL\">";
+                            echo "<h2>You are an:</h2>";
+                            echo "<input type=\"radio\" name=\"individualOrOrganization\" value=\"individual\"> Individual";
+                            echo "<input type=\"radio\" name=\"individualOrOrganization\" value=\"organization\"> Organanization";
+                        } else {
+                            echo "<input type=\"password\" id=\"input-name\" placeholder=\"Password\">";
+                        }
+                    ?>
+                    <?php
+                        if (($forgotPassword) || ($requestInvitation)) {
+                            echo "";
+                        } else {
+                            echo "<div id=\"rememberMeSection\"><input type=\"checkbox\" checked/><label for=\"rememberMe\">Remember Me</label></div>";
+                        }
+                    ?>
+
                     <input type="submit" value="Submit" id="submitButton">
                     <div id="additionalOptions">
-                        <a href="" id="requestInvitation">Request Invitation</a>
-                        <a href="" id="forgotPassword">Forgot Password</a>
+                        <button id="requestInvitationBtn" class="alternativeOptions" type="submit" name="requestInvitation">Request Invitation</button>
+                        <?php
+                            if ($forgotPassword) {
+                                echo 
+                                    "<button id=\"alternativeTwoBtn\" class=\"alternativeOptions\" type=\"submit\" name=\"login\">Login</button>";
+                            } else {
+                                echo "<button id=\"alternativeTwoBtn\" class=\"alternativeOptions\" type=\"submit\" name=\"forgotPassword\">Forgot Password</button>";
+                            }
+                        ?>
                     </div>
                 </div>
             </form>
