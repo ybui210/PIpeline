a51e94de0993e1f7dd2d1be7457dab88a6fae3a4
aavram <aavram27@gmail.com>
2017-11-05 01:27:28 -0800

    Revised home page mobile

--- Current-Version/home.php
+++ Current-Version/home.php
@@ -6,6 +6,7 @@
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
@@ -40,6 +41,38 @@
         </div>-->
         <div class="container-fluid" >
           <div class="row">
+              <nav class="navbar navbar-default">
+                  <div class="container-fluid">
+                      <!-- Brand and toggle get grouped for better mobile display -->
+                      <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                              <span class="sr-only">Toggle navigation</span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                          </button>
+                          <a class="navbar-brand" href="#">Pipeline</a>
+                      </div>
+
+                      <!-- Collect the nav links, forms, and other content for toggling -->
+                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                          <ul class="nav navbar-nav">
+                              <li class="active"><a href="#">Browse Listing <span class="sr-only">(current)</span></a></li>
+                              <li><a href="#">Active Listing</a></li>
+                              <li><a href="#">Create Listing</a></li>
+                              <li><a href="#">News</a></li>
+
+                          </ul>
+                          <form class="navbar-form navbar-left">
+                              <div class="form-group">
+                                  <input type="text" class="form-control" placeholder="Search">
+                              </div>
+                              <button type="submit" class="btn btn-default">Submit</button>
+                          </form>
+                      </div><!-- /.navbar-collapse -->
+                  </div><!-- /.container-fluid -->
+              </nav>
+              <!--
               <nav class="navbar navbar-inverse topNavBarDiv">
                   <div class="container-fluid">
                       <div class="navbar-header">
@@ -58,29 +91,33 @@
                           <button type="submit" class="btn btn-default">Submit</button>
                       </form>
                   </div>
-              </nav>
+              </nav>-->
 
           </div>
 
           <div class="row">
-            <div class="col-sm-3 col-lg-2 navBarDiv">
-              <nav class="nav nav-pills nav-stacked leftNavbar">
-                <li><a href="profile.php">Profile</a></li>
-                <li class="active"><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="myListings.php">My Listings</a></li>
-                <li><a href="savedListings.php">Saved Listings</a></li>
-                <li><a href="">Drafts</a></li>
-              </nav>
-            </div>
-            <div class="col-sm-9 col-lg-10">
-                <div class="row header">
-                
-                </div>
-              <!-- your page content -->
+            <div class="col-sm-3 col-lg-2 navBarDiv hidden-xs">
+
+                <nav class="nav nav-pills nav-stacked leftNavbar">
+                  <li><a href="">Account</a></li>
+                  <li class="active"><a href="">Password</a></li>
+                  <li><a href="">Profile</a></li>
+                  <li><a href="">Notifications</a></li>
+                  <li><a href="">System History</a></li>
+                    <li><a href="">Social Connections</a></li>
+                </nav>
+              </div>
+              <div class="col-sm-9 col-lg-10">
+                  <div class="row header">
+
+                  </div>
+                <!-- your page content -->
             </div>
           </div>
         </div>
-              <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     </body>
-  </html> 
+  </html>
