9afcbab1daf294819667eaf6439f3e069a23721b
Yen Bui <ybui0210@gmail.com>
2017-11-12 13:19:58 -0800

    added listing pages

---
+++ Current-Version/searchListings.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link href="Styles/home.css" rel="stylesheet" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+<!--Import jQuery before materialize.js-->
+
+
+<!--
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo right">Logo</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">My Listings</a></li>
+                    <li><a href="">Saved Listings</a></li>
+                    <li><a href="">Drafts</a></li>
+                </ul>
+            </div>
+        </nav>
+-->
+
+<!--<div class="w3-sidebar w3-bar-block" style="width:25%">
+    <a href="#" class="w3-bar-item w3-button">Link 1</a>
+    <a href="#" class="w3-bar-item w3-button">Link 2</a>
+    <a href="#" class="w3-bar-item w3-button">Link 3</a>
+</div>
+
+<div style="margin-left:25%">
+    ... page content ...
+</div>-->
+<div class="container-fluid" >
+    <div class="row">
+        <nav class="navbar navbar-inverse topNavBarDiv">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Pipeline</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Browse Listings</a></li>
+                    <li><a href="#">Active Listings</a></li>
+                    <li><a href="createListing.php">Create Listing</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-3 col-lg-2 navBarDiv">
+            <nav class="nav nav-pills nav-stacked leftNavbar">
+                <li><a href="profile.php">Profile</a></li>
+                <li class="active"><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="myListings.php">My Listings</a></li>
+                <li><a href="savedListings.php">Saved Listings</a></li>
+                <li><a href="">Drafts</a></li>
+            </nav>
+        </div>
+        <div class="col-sm-6 col-lg-10">
+            <h1>Result of </h1>
+            
+
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+</body>
+</html>
