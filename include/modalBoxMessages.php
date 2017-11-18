aa7465268ea8bca6a10fa23e57d3b434694a0d60
Davin Deol <davindeol@gmail.com>
2017-11-17 18:50:00 -0800

    Moved Favicon and Modal Box Messages in include folder

---
+++ include/modalBoxMessages.php
@@ -0,0 +1,26 @@
+<?php
+    function displayBlacklistMessage() {
+        $result = "<script>alert(\"You are on our blacklist\")</script>";
+        return $result;
+    }
+    function displayWarningMessage() {
+        $result = "<script>alert(\"You've been sending too many messages. Please try again tomorrow.\")</script>";
+        return $result;
+    }
+    function displayRequestSentMessage() {
+        $result = "<script>alert(\"Request has been sent.\")</script>";
+        return $result;
+    }
+    function displayYouDoNotHaveAnAccountMessage() {
+        $result = "<script>alert(\"You Do Not Have An Account With Us.\")</script>";
+        return $result;
+    }
+    function displayPasswordSentMessage() {
+        $result = "<script>alert(\"Password has been sent to you.\")</script>";
+        return $result;
+    }
+    function displayMessage($msg) {
+        $result = "<script>alert(" . $msg . ")</script>";
+        return $result;
+    }
+?>
