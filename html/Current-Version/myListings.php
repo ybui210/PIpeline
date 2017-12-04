789d09a86c9c33df65cafdd544aea559755ae22e
Davin Deol <davindeol@gmail.com>
2017-12-04 01:42:58 -0800

    Reconfigured navbars

--- html/Current-Version/myListings.php
+++ html/Current-Version/myListings.php
@@ -1,27 +1,15 @@
 <?php 
+    require_once("../../include/favicon.php");
     require_once("../../include/configdb.php");
-    //require_once("../configdb.php");
-
-    session_start();
-
-    $admin = false;
-    $userEmail = $_SESSION["userEmail"];
-    
-
-$dir    = 'Uploads/';
-$files = scandir($dir);
-$sql = "SELECT type FROM Users WHERE email='.$userEmail.'";
-$result = $link->query($sql);
-$row = $result->fetch_array(MYSQLI_ASSOC);
-$userType = $row["type"];
-if ($userType == "admin") {
-    $admin = true;
-}
-
+    require_once("../../include/navBar.php");
+    require_once("../../include/getUserTypeAndVerifyLogin.php");
+    $dir    = 'Uploads/';
+    $files = scandir($dir);
 ?>
 <!DOCTYPE html>
 <html>
-<meta name="viewport" content="width=device-width, initial-scale=1">    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <head>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="Styles/home.css" rel="stylesheet" />
@@ -29,49 +17,22 @@ if ($userType == "admin") {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
 
-<div class="container-fluid" >
-    <div class="row">
-        <nav class="navbar navbar-inverse topNavBarDiv" style="margin-bottom: 0">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Pipeline</a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Browse Listings</a></li>
-                    <li><a href="latestListings.php">Active Listings</a></li>
-                    <li><a href="createListing.php">Create Listing</a></li>
-                    <li><a href="#">News</a></li>
-                </ul>
-                <form class="navbar-form navbar-left" method="post" action="searchListings.php">
-                    <div class="form-group">
-                        <input name="searchkey" type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
-            </div>
-        </nav>
-    </div>
+    <div class="container-fluid">
+        <?php displayNavBar($userType); ?>
 
-    <div class="row">
-        <div class="col-sm-3 col-lg-2 navBarDiv nav-pills nav-stacked">
-            <nav class="nav nav-pills nav-stacked leftNavbar">
-                <li><a href="profile.php">Profile</a></li>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li class="active"><a href="myListings.php">My Listings</a></li>
-                <li><a href="savedListings.php">Saved Listings</a></li>
-                <li><a href="">Drafts</a></li>
-            </nav>
-        </div>
-        
-        <!************************************/>
-        <div class="col-sm-6 col-lg-10">
-            <h1>My Listings</h1>
-            <?php
+        <div class="row">
+
+            <?php displaySideBar("My Listings", $userType); ?>
+
+            <!************************************/>
+            <div class="col-sm-6 col-lg-10">
+                <h1>My Listings</h1>
+                <?php
             $sql = "SELECT listingId, name, introduction FROM Listings;";
             $result = $link->query($sql);
             //for ($i=0; $i<5; $i++){
@@ -91,7 +52,7 @@ if ($userType == "admin") {
                 }
                 if ($intro != null){
                 ?>
-                <a style="display:block; text-decoration:none; marginTop:10" href="<?php $_SESSION["view"]=1; echo "reviewListing.php?id=".$id;?>">
+                    <a style="display:block; text-decoration:none; marginTop:10" href="<?php $_SESSION["view"]=1; echo "reviewListing.php?id=".$id;?>">
                 <div class="row" style="marginTop:10">
                     <div class="w3-card-4" margin="10">
 
@@ -116,16 +77,13 @@ if ($userType == "admin") {
                         
                 </div>
             </a>
-
-                
-            <?php
+                    <?php
             }}
             ?>
             </div>
         </div>
     </div>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </body>
+
 </html>
